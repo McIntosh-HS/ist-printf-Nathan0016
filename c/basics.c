0445677c6897107f78ba022238e02dd0b74de7bd
Nathan Spillner <2027spillner.nathan@fcboe.org>
2024-01-24 15:33:54 -0500

    finished

---
+++ c/basics.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main () {
+	
+
+printf ("Hello me");
+
+int num;	// type name;
+printf ("enter a numberi: ");	// prompt user for a number
+scanf("%i", &num);	// store user input in num
+printf("You entered %i", num);
+	return 0;
+}
---
+++ c/scanf.c
@@ -0,0 +1,20 @@
+
+
+
+#include <stdio.h>
+int main (){
+
+int age;
+float gpa;
+char grade;
+
+printf("what is your age");
+scanf("%i", &age);
+printf("what is your gpa");
+scanf("%f", &gpa);
+printf("what is your grade");
+scanf("%c", &grade);
+printf( "you are is %i, your gpa is %f your grade is %c", age, gpa, grade);
+return 0;
+}
+
